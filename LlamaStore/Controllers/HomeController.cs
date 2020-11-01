2bf79a9b0c483c30b74081227207a391edb4f29b
unknown <shayan.khademi.1994@gmail.com>
2020-11-01 19:49:35 +0100

    Dependency Injection

---
+++ LlamaStore/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace LlamaStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IConfiguration configuration;
+        private readonly ILogger<HomeController> logger;
+        public HomeController(IConfiguration configuration,ILogger<HomeController> logger)
+        {
+            this.configuration = configuration;
+            this.logger = logger;
+
+            logger.LogInformation($"NavBar color: {configuration["NavBar"]}");
+            logger.LogInformation($"Store name: {configuration["StoreName"]}");
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult BrowserInfo()
+        {
+            string userAgent = Request.Headers["User-Agent"].ToString();
+            ViewBag.UserAgent = userAgent;
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
---
+++ LlamaStore/Controllers/ProductController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LlamaStore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LlamaStore.Controllers
+{
+    public class ProductController : Controller
+    {
+        private  List<Product> products = new List<Product>
+            {
+                new Product {Id=1,Name="Fluffy Llama",Description="A fluffy Llama",Price=9.99M },
+                new Product {Id=2,Name="Colorful Llama",Description="A Colorful Llama",Price=19.99M }
+            };
+        public IActionResult Index()
+        {
+            return View(products);
+        }
+        public IActionResult Details(int id)
+        {
+            foreach (var product in products)
+            {
+                if (product.Id == id)
+                {
+                    return View(product);
+                }
+            }
+            return NotFound();
+        }
+    }
+}
